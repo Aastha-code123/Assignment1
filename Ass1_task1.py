b9d797dd3cc3efad43802a4d75d254717d455dfa
aastha garg <gargaastha811@gmail.com>
2025-12-10 20:40:17 +0530

    Added Task1 and Task2 python files

---
+++ Ass1_task1.py
@@ -0,0 +1,10 @@
+# taking input from user
+a = int(input("Enter the first number : "))
+b = int(input("Enter the second number : "))
+
+# perform operation
+print("Addition : ",a+b)
+print("Subtraction : ",a-b)
+print("Multiplication : ",a*b)
+print("Division : ",a/b)
+
